72c438670a3c0d21ac5446930002261600ae7074
Ben61183 <Ben61183@GB-LD1LPW10G022.mastek.com>
2019-07-29 16:45:06 +0100

    assign role postman

--- src/main/java/com/mastek/jobsapp/apis/VacancyService.java
+++ src/main/java/com/mastek/jobsapp/apis/VacancyService.java
@@ -1,19 +1,31 @@
 package com.mastek.jobsapp.apis;
 
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.mastek.jobsapp.entities.Role;
 import com.mastek.jobsapp.entities.Vacancy;
 import com.mastek.jobsapp.repositories.VacancyRepository;
 
 @Component
 @Scope("singleton")
+@Path("/vacancy/")
 public class VacancyService {
-		
 		
 		@Autowired
 		private VacancyRepository vacancyRepository;
+		
+		@Autowired
+		private RoleService rolSer;
 		
 //		
 //		private Team currentTeam;
@@ -51,6 +63,25 @@ public class VacancyService {
 //			this.currentTeam = currentTeam;
 //		}
 
+		
+	@Transactional
+	@POST
+	@Path("/assign/role")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Vacancy assignRole(@FormParam("roleId") int roleId, @FormParam("vacancyId") int vacancyId) {
+		try {
+			Vacancy vac = findByVacanyId(vacancyId);
+			Role rol = rolSer.findByRoleId(roleId);
+			vac.setThisRole(rol);
+			vac = registerOrUpdateVacancy(vac);
+			return vac;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+			
+		}
 
 
 	}
