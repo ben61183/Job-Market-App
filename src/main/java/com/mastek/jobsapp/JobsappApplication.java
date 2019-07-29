50e709d0547a50f50e8a556db21a5c8947967bb4
Ben61183 <Ben61183@GB-LD1LPW10G022.mastek.com>
2019-07-29 10:19:52 +0100

    another test for GITHUB

--- src/main/java/com/mastek/jobsapp/JobsappApplication.java
+++ src/main/java/com/mastek/jobsapp/JobsappApplication.java
@@ -9,6 +9,7 @@ public class JobsappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JobsappApplication.class, args);
 //		System.out.println("github?");
+		// test
 	}
 
 }
