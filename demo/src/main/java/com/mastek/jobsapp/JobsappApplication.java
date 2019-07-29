f6c274b1b17f8e40110292cbee2e86ca16072c51
Ben61183 <Ben61183@GB-LD1LPW10G022.mastek.com>
2019-07-29 10:05:32 +0100

    sd

--- demo/src/main/java/com/mastek/jobsapp/JobsappApplication.java
+++ demo/src/main/java/com/mastek/jobsapp/JobsappApplication.java
@@ -8,6 +8,7 @@ public class JobsappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobsappApplication.class, args);
+		System.out.println("connected to git?");
 	}
 
 }
