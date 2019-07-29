0f1f81d4214932843596d1221cfdc2cd8c88e9fb
Ben61183 <Ben61183@GB-LD1LPW10G022.mastek.com>
2019-07-29 10:21:24 +0100

    return to default after github setup tests

--- src/main/java/com/mastek/jobsapp/JobsappApplication.java
+++ src/main/java/com/mastek/jobsapp/JobsappApplication.java
@@ -8,8 +8,6 @@ public class JobsappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobsappApplication.class, args);
-//		System.out.println("github?");
-		// test
 	}
 
 }
