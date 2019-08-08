4b0aeb654d6f9f7e2b83e4e85aac5f89ed97dad1
Ben61183 <Ben61183@GB-LD1LPW10G022.mastek.com>
2019-08-08 16:01:50 +0100

    user changes

--- src/main/java/com/mastek/jobsapp/apis/UserDetailsService.java
+++ src/main/java/com/mastek/jobsapp/apis/UserDetailsService.java
@@ -5,8 +5,10 @@ import java.util.Set;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -18,7 +20,6 @@ import com.mastek.jobsapp.entities.Skill;
 import com.mastek.jobsapp.entities.User;
 import com.mastek.jobsapp.entities.Vacancy;
 import com.mastek.jobsapp.repositories.UserRepository;
-
 
 
 @Component
@@ -46,7 +47,11 @@ public class UserDetailsService {
 		return usr;
 	}
 	
-	public User findByUserId(int userId) {
+	@Transactional
+	@GET
+	@Path("/find/{userId}")
+	@Produces({MediaType.APPLICATION_JSON})
+	public User findByUserId(@PathParam("userId") int userId) {
 		try {
 			return userRepository.findById(userId).get();
 		} catch (Exception e) {
@@ -92,6 +97,46 @@ public class UserDetailsService {
 			Vacancy s = vacSer.findByVacanyId(vacancyId);
 			// manage the association
 			u.getSavedVacancies().add(s); // one assigned with many
+			u = registerAccountOrUpdate(u);
+			return u;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	@Transactional
+	@POST
+	@Path("/delete/skill")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+		public User deleteSkill(@FormParam("userId") int userId, @FormParam("skillId") int skillId) {
+		try {
+			// fetch entities to be associated
+			User u = findByUserId(userId);
+			Skill s = skiSer.findBySkillId(skillId);
+
+			u.getUserSkills().remove(s); 
+			u = registerAccountOrUpdate(u);
+			return u;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	@Transactional
+	@POST
+	@Path("/delete/vacancy")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+		public User deleteVacancy(@FormParam("userId") int userId, @FormParam("vacancyId") int vacancyId) {
+		try {
+			// fetch entities to be associated
+			User u = findByUserId(userId);
+			Vacancy v = vacSer.findByVacanyId(vacancyId);
+
+			u.getSavedVacancies().remove(v); 
 			u = registerAccountOrUpdate(u);
 			return u;
 		} catch(Exception e) {
--- src/main/java/com/mastek/jobsapp/entities/User.java
+++ src/main/java/com/mastek/jobsapp/entities/User.java
@@ -14,6 +14,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.ws.rs.Path;
 import javax.xml.bind.annotation.XmlTransient;
 
 import org.springframework.context.annotation.Scope;
